953614b4e4a2aba80434036d0c92596c2aec3035
rodrigoacm10 <rodrigoacm10@gmail.com>
2024-07-03 06:21:26 -0300

    table next

---
+++ src/app/users/page.tsx
@@ -0,0 +1,25 @@
+import { DataTable } from "@/components/data-table";
+import { User, columns } from "./columns";
+
+async function getUsers(): Promise<User[]> {
+  const res = await fetch(
+    "https://64a6f5fc096b3f0fcc80e3fa.mockapi.io/api/users"
+  );
+
+  const data = await res.json();
+
+  return data;
+}
+
+export default async function Page() {
+  const data = await getUsers();
+
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h1 className="mb-6 text-3xl font-bold">All users</h1>
+        <DataTable columns={columns} data={data} />
+      </div>
+    </section>
+  );
+}
